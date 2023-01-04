ee675b599590d59c7e92bdb2d25a078a59952a9e
IzzuddinAzzaky <113503305+IzzuddinAzzaky@users.noreply.github.com>
2023-01-04 23:14:17 +0700

    Add files via upload

---
+++ src/test/java/com/izzuddin/perpustakaan/PerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzuddin.perpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
